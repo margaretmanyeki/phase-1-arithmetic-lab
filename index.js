ff24c9777673d9d897853002d752b2ad870640bf
margaret manyeki <maggiemanyeki6@gmail.com>
2022-06-08 12:19:57 +0300

    commit tree 18a5dbe7d3586d211bfed91863647a9cc98c7b49

--- index.js
+++ index.js
@@ -1,10 +1,2 @@
- 
-let num1=31,
-    num2=2,
-    num3=31,
-    num4=9;
-
-let multiply = num1 * num2;
-let random = Math.floor(Math.random()*10)+1;
-let mod =num3%num4;
-let max = Math.max(1,2,3,4,5,6,7,17,19,20);
+let multiply = (num1,num2)=> num1*num2
+multiply(31,2)
